1a4c3e261763cc845e16fa69d9009ebbe915162b
parkjong0328 <parkjong0328@gmai.com>
2024-04-19 19:21:43 +0900

    테스트

---
+++ 과제#2 heap/MinHeap_test.py
@@ -0,0 +1,12 @@
+from MinHeap import MinHeap
+
+minheap = MinHeap()
+
+# 요소 삽입
+minheap.insert('a', 10)
+minheap.insert('b', 5)
+minheap.insert('c', 8)
+
+print("최소값", minheap.min())  # 예상 출력: ('b', 5)
+print("요소 삭제:", minheap.deleteMin())  # 예상 출력: ('b', 5)
+print("삭제후 최소값 확인:", minheap.min())  # 예상 출력: ('c', 8)
